9308cf08c6aea5496d1418276f9ba75613b9f260
Daniel Greenfeld <pydanny@gmail.com>
2012-02-29 14:25:04 -0800

    Adding tests

--- cn_client/functions.py
+++ cn_client/functions.py
@@ -5,16 +5,17 @@ cn_client.functions
 ~~~~~~~~~~~~~~~~~~~
 
 This module provides the basic API interface for Consumer Notebook. The 
-abstraction of this is super-light in order to show how to 
+abstraction of this is super-light in order to show how to use the API.
 """
 
 import json
 
 import requests
 
-CONSUMERNOTEBOOK_URL = "https://api.consumernotebook.com/api/v1/"
+#CONSUMERNOTEBOOK_URL = "https://consumernotebook.com/api/v1/"
+CONSUMERNOTEBOOK_URL = "https://127.0.0.1/api/v1/"
 
-def products(api_key, username, password, page=1,from_date=None, to_date=None):
+def get_products(api_key, username, password, page=1,from_date=None, to_date=None):
     """ Returns a ``requests`` response object"""
     data = {}
     data['page'] = page
@@ -23,7 +24,7 @@ def products(api_key, username, password, page=1,from_date=None, to_date=None):
     url = CONSUMERNOTEBOOK_URL + 'products/'
     return request.get(url, auth=(username, password), data=data)
 
-def lists(api_key, username, password, page=1,from_date=None, to_date=None, depth=0):
+def get_lists(api_key, username, password, page=1,from_date=None, to_date=None, depth=0):
     """ Returns a ``requests`` response object"""
     data = {}
     data['page'] = page
@@ -33,7 +34,7 @@ def lists(api_key, username, password, page=1,from_date=None, to_date=None, dept
     url = CONSUMERNOTEBOOK_URL + 'lists/'    
     return request.get(url, auth=(username, password), data=data)
 
-def grids(api_key, username, password, page=1,from_date=None, to_date=None, depth=0):
+def get_grids(api_key, username, password, page=1,from_date=None, to_date=None, depth=0):
     """ Returns a ``requests`` response object"""
     data = {}
     data['page'] = page
