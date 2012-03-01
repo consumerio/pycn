38db2e370fa5e039cc7ede0fb30d3f751fc2a663
Daniel Greenfeld <pydanny@gmail.com>
2012-03-01 11:38:38 -0800

    Add some more docs

--- cn_client/functions.py
+++ cn_client/functions.py
@@ -4,14 +4,20 @@
 cn_client.functions
 ~~~~~~~~~~~~~~~~~~~
 
-This module provides the basic API interface for Consumer Notebook. The 
+This module provides a basic API interface for Consumer Notebook. The 
 abstraction of this is super-light in order to show how to use the API.
+
+All Functions return a python-requests response object. It is up to the
+user of this API to convert that to Python data
 """
 
 import requests
 
 def get_products(api_key, username, password, page=1,from_date=None, to_date=None, base_url="https://consumernotebook.com/api/v1/"):
-    """ Returns a ``requests`` response object"""
+    """ 
+        Returns a requests response object containing a list of serialized
+        Consumer Notebook product data.
+    """
     data = {}
     data['api_key'] = api_key    
     data['page'] = page
@@ -21,7 +27,10 @@ def get_products(api_key, username, password, page=1,from_date=None, to_date=Non
     return requests.get(url, auth=(username, password), data=data)
 
 def get_lists(api_key, username, password, page=1,from_date=None, to_date=None, depth=0, base_url="https://consumernotebook.com/api/v1/"):
-    """ Returns a ``requests`` response object"""
+    """ 
+        Returns a requests response object containing a list of serialized
+        Consumer Notebook list data.
+    """
     data = {}
     data['page'] = page
     data['from_date'] = from_date
@@ -31,7 +40,10 @@ def get_lists(api_key, username, password, page=1,from_date=None, to_date=None, 
     return requests.get(url, auth=(username, password), data=data)
 
 def get_grids(api_key, username, password, page=1,from_date=None, to_date=None, depth=0, base_url="https://consumernotebook.com/api/v1/"):
-    """ Returns a ``requests`` response object"""
+    """ 
+        Returns a requests response object containing a list of serialized
+        Consumer Notebook grid data.
+    """
     data = {}
     data['page'] = page
     data['from_date'] = from_date
