d0507dbc79c922700389ee3545f1e8062c41089b
Daniel Greenfeld <pydanny@gmail.com>
2012-03-01 15:06:12 -0800

    Function cleanup

--- cn_client/functions.py
+++ cn_client/functions.py
@@ -13,7 +13,7 @@ user of this API to convert that to Python data
 
 import requests
 
-def get_products(api_key, username, password, page=1,from_date=None, to_date=None, base_url="https://consumernotebook.com/api/v1/"):
+def get_products(api_key, username, password, target_username=None, page=1,from_date=None, to_date=None, base_url="https://consumernotebook.com/api/v1/"):
     """ 
         Returns a requests response object containing a list of serialized
         Consumer Notebook product data.
@@ -23,10 +23,12 @@ def get_products(api_key, username, password, page=1,from_date=None, to_date=Non
     data['page'] = page
     data['from_date'] = from_date
     data['to_date'] = to_date
+    if target_username:
+        data['username'] = target_username    
     url = base_url + 'products.json'
     return requests.get(url, auth=(username, password), data=data)
 
-def get_lists(api_key, username, password, page=1,from_date=None, to_date=None, depth=0, base_url="https://consumernotebook.com/api/v1/"):
+def get_lists(api_key, username, password, target_username=None, page=1,from_date=None, to_date=None, depth=0, base_url="https://consumernotebook.com/api/v1/"):
     """ 
         Returns a requests response object containing a list of serialized
         Consumer Notebook list data.
@@ -37,10 +39,12 @@ def get_lists(api_key, username, password, page=1,from_date=None, to_date=None, 
     data['from_date'] = from_date
     data['to_date'] = to_date
     data['depth'] = depth    
+    if target_username:
+        data['username'] = target_username    
     url = base_url + 'lists.json'    
     return requests.get(url, auth=(username, password), data=data)
 
-def get_grids(api_key, username, password, page=1,from_date=None, to_date=None, depth=0, base_url="https://consumernotebook.com/api/v1/"):
+def get_grids(api_key, username, password, target_username=None, page=1,from_date=None, to_date=None, depth=0, base_url="https://consumernotebook.com/api/v1/"):
     """ 
         Returns a requests response object containing a list of serialized
         Consumer Notebook grid data.
@@ -51,5 +55,23 @@ def get_grids(api_key, username, password, page=1,from_date=None, to_date=None, 
     data['from_date'] = from_date
     data['to_date'] = to_date
     data['depth'] = depth    
+    if target_username:
+        data['username'] = target_username    
+    url = base_url + 'grids.json'    
+    return requests.get(url, auth=(username, password), data=data)
+
+def get_users(api_key, username, password, target_username=None, page=1,base_url="https://consumernotebook.com/api/v1/"):
+    """ 
+        Returns a requests response object containing a list of serialized
+        Consumer Notebook grid data.
+    """
+    data = {}
+    data['api_key'] = api_key       
+    data['page'] = page
+    data['from_date'] = from_date
+    data['to_date'] = to_date
+    data['depth'] = depth 
+    if target_username:
+        data['username'] = target_username
     url = base_url + 'grids.json'    
     return requests.get(url, auth=(username, password), data=data)
